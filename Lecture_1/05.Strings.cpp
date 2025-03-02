c7c9c62b5fdcb5938442263355ec882dbda1e87c
Poorvanshi sahu <65068317+Poorvanshi-sahu@users.noreply.github.com>
2025-03-02 21:10:20 +0000

    first lecture added

---
+++ Lecture_1/05.Strings.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   
+    string s = "Poorvanshi";
+
+    cout<<s.size();
+
+    s[s.size()-1] = 's';
+
+    cout<<s;
+
+    cout<<"\n\n\n\n";
+    return 0;
+}
---
+++ Lecture_1/06.loop.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   
+    for(int i=0;i<10;i++){
+        cout<<"Striver"<<endl;
+    }
+
+    cout<<"\n\n\n\n";
+    return 0;
+}
