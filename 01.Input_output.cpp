4d7feac983d52d09edcb2c4a2696b0d52f3675d8
Poorvanshi sahu <65068317+Poorvanshi-sahu@users.noreply.github.com>
2025-03-02 12:01:53 +0000

    if else data type added

--- 01.Input_output.cpp
+++ 01.Input_output.cpp
@@ -1,8 +1,18 @@
-#include<iostream>
+// #include<iostream>
+#include<bits/stdc++.h> // for all library
 // #include<math.h>  for math
 // #include<string>  for string
+using namespace std;
 
 int main(){
-    std::cout<<"Hey string";
+    // without namespace std
+    // std::cout<<"Hey string";
+
+    // with namespace
+    int x, y;
+    cin>>x>>y;
+    cout<<"Value of x: "<<x<<" and value of y: "<<y;
+
+    cout<<"\n\n\n\n";
     return 0;
 }
---
+++ 02.Datatypes.cpp
@@ -0,0 +1,29 @@
+// #include<iostream>
+#include<bits/stdc++.h> // for all library
+// #include<math.h>  for math
+// #include<string>  for string
+using namespace std;
+
+int main(){
+
+//  int -10^9 to 10^9
+//  long -10^12 to 10^12
+//  long long -10^18 to 10^18
+
+
+    // long x;
+    // long long x;
+    // float x = 3.3;
+    // double x;
+    // string s:     //string with take anything before space and getline will take complete line
+
+    // string str;
+    // getline(cin, str);
+    // cout<<str;
+
+    // char ch;
+
+
+    cout<<"\n\n\n\n";
+    return 0;
+}
